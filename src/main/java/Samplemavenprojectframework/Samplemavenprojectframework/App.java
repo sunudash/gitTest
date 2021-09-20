72d59ebb88db86ef07b8453ec8c310ef5736e398
sunudash <91034937+sunudash@users.noreply.github.com>
2021-09-20 21:36:57 +0530

    Add files via upload

---
+++ src/main/java/Samplemavenprojectframework/Samplemavenprojectframework/App.java
@@ -0,0 +1,13 @@
+package Samplemavenprojectframework.Samplemavenprojectframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
